c94cabe6a3c63754fd1ae72055264c8c8692a294
DangReceiver <TearsDontFall.TDF@gmail.com>
2023-05-13 16:47:20 +0200

    Java 17 -> Java 18

--- src/main/java/de/tdf/gokart/Hotbar.java
+++ src/main/java/de/tdf/gokart/Hotbar.java
@@ -7,17 +7,18 @@ import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedMainHandEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
 public class Hotbar implements Listener {
 
 	@EventHandler
-	public void handle(PlayerInventorySlotChangeEvent e) {
+	public void handle(PlayerItemHeldEvent e) {
 		Player p = e.getPlayer();
-		int rawSlot = e.getRawSlot(),
-				slot = e.getSlot();
-		int difference = rawSlot - slot;
+		int newSlot = e.getNewSlot();
+		int previousSlot = e.getPreviousSlot();
+		int difference = previousSlot - newSlot;
 
-		p.sendMessage(Language.PRE + String.format("raw: %s; slot: %s | difference: %s", rawSlot, slot, difference));
+		p.sendMessage(Language.PRE + String.format("raw: %s; slot: %s | difference: %s", newSlot, previousSlot, difference));
 	}
 }
